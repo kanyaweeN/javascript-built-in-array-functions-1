f39c1de3f39fd1f2ad8bcfeb94fb30f1db248fd4
Naranut05 <naranut.sresroy@gmail.com>
2025-07-25 15:28:38 +0700

    Assignment

--- ex-5.js
+++ ex-5.js
@@ -6,7 +6,10 @@ const students = [
 ];
 
 function getAverageStudentScore(students) {
-  // Start coding here
+  const totalScore = students.reduce(function (total, student) {
+    return total + student.score;
+  }, 0);
+  return totalScore / students.length;
 }
-
 getAverageStudentScore(students); // Output: 87.5
+console.log(getAverageStudentScore(students));
